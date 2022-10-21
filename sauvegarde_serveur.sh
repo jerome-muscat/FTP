4de751afb717af8e868f7706089730dfbd1bbe1b
jerome-muscat <jerome.muscat@laplateforme.io>
2022-10-21 18:19:23 +0200

    Job Pour Allée plus loin ...

--- sauvegarde_serveur.sh
+++ sauvegarde_serveur.sh
@@ -1,6 +1,6 @@
 #cette commande permet d'afficher la date selon le format que l'on souhaite, ici : jj-mm-aaaa-HH-MM, et l'attribut à une variable précédemment nommé.
 d=$(date +%d-%m-%Y-%H-%M)
 
-sudo tar -czf /home/jerome/backup-$d.tar.gz /home /etc/proftpd
+sudo tar -czf /home/jerome/backup-$d.tar.gz /home /etc/proftpd 
 
 lftp ftp://jerome:jerome@192.168.31.134 -e "put -O /home/jerome/ /home/jerome/backup-$d.tar.gz; quit"
