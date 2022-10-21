92e4a782eb0f360990776dc9fb1463dafa34280e
jerome-muscat <jerome.muscat@laplateforme.io>
2022-10-21 18:24:57 +0200

    Job 8

--- suppresession_serv.sh
+++ suppresession_serv.sh
@@ -3,5 +3,6 @@
 sudo rm /etc/proftpd/ssl/proftpd.cert.pem && sudo rm /etc/proftpd/ssl/proftp.key.pem
 rm -r /home/Mery
 rm -r /home/Pippin
+
 sudo userdel Merry && sudo userdel Pippin
-sudo apt --purge remove proftpd-*
+sudo apt --purge remove proftpd-* && sudo apt --purge remove openssh-server
