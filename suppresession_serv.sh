b0e6585826d9fafbe2f53514736f12f00a445908
jerome-muscat <jerome.muscat@laplateforme.io>
2022-10-22 13:19:53 +0200

    Delete suppresession_serv.sh

--- suppresession_serv.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sudo rm /etc/proftpd/ssl/proftpd.cert.pem && sudo rm /etc/proftpd/ssl/proftp.key.pem
-rm -r /home/Mery
-rm -r /home/Pippin
-
-sudo userdel Merry && sudo userdel Pippin
-sudo apt --purge remove proftpd-* && sudo apt --purge remove openssh-server
