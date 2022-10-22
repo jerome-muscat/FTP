648664efc1cb1d569e13dc24f1192d33fbc9ac77
jerome-muscat <jerome.muscat@laplateforme.io>
2022-10-22 13:29:19 +0200

    Script Job 8

--- suppression_serv.sh
+++ suppression_serv.sh
@@ -2,10 +2,10 @@
 
 #rm permet de supprimer des fichiers. On peut lui assigner l'option -r pour pouvoir supprimer des dossiers.
 sudo rm /etc/proftpd/ssl/proftpd.cert.pem && sudo rm /etc/proftpd/ssl/proftp.key.pem
-rm -r /home/Mery
-rm -r /home/Pippin
 
-#si on le souhaite, on peut supprimer des utilisateurs.
+#si on le souhaite, on peut supprimer des utilisateurs et leurs home.
+#rm -r /home/Mery
+#rm -r /home/Pippin
 #sudo userdel Merry && sudo userdel Pippin
 
 #apt --purge remove permet de désinstaller un ou plusieurs paquets.
