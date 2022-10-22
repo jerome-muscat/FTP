59fdaaa447a0fd310ba946f19c45b4f112590e90
jerome-muscat <jerome.muscat@laplateforme.io>
2022-10-22 13:14:05 +0200

    Script du Job 8

---
+++ suppression_serv.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#rm permet de supprimer des fichiers. On peut lui assigner l'option -r pour pouvoir supprimer des dossiers.
+sudo rm /etc/proftpd/ssl/proftpd.cert.pem && sudo rm /etc/proftpd/ssl/proftp.key.pem
+rm -r /home/Mery
+rm -r /home/Pippin
+
+#userdel permet de supprimer des utilisateurs
+sudo userdel Merry && sudo userdel Pippin
+
+#apt --purge remove permet de désinstaller un ou plusieurs paquets
+sudo apt --purge remove proftpd-* 
+
+#si on le souhaite, on peut désinstaller les paquets openssh et openssl
+#sudo apt --purge remove openssh-server
+#sudo apt --purge remove openssl
